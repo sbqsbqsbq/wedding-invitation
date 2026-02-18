5814757180235b82e0c1a1809bfafbf1b1fc0d20
sbqsbqsbq <kimjohn2973@gmail.com>
2026-02-18 10:39:01 +0900

    fix: nextjs 버전 변경

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2025-06-29T09:07:01.917Z
+// Build Time: 2026-02-18T01:31:51.784Z
 export const APP_VERSION = {
-  version: '1751188021917',
-  buildTime: '2025-06-29T09:07:01.917Z',
-  hash: 'q4ps0k5d4b'
+  version: '1771378311784',
+  buildTime: '2026-02-18T01:31:51.784Z',
+  hash: 'en9joovha2v'
 };
 
 export default APP_VERSION;
