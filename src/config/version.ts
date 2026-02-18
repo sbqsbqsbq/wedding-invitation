1f27f765a5c4ede808da943388e8ef0ad30a0b6a
sbqsbqsbq <kimjohn2973@gmail.com>
2026-02-18 11:20:49 +0900

    feat: 영문 페이지

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2026-02-18T02:17:15.739Z
+// Build Time: 2026-02-18T02:20:39.650Z
 export const APP_VERSION = {
-  version: '1771381035739',
-  buildTime: '2026-02-18T02:17:15.739Z',
-  hash: 'w9hsxa35ygg'
+  version: '1771381239650',
+  buildTime: '2026-02-18T02:20:39.650Z',
+  hash: 'gi84serhlc6'
 };
 
 export default APP_VERSION;
